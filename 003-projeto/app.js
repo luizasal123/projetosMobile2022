4818e5b96158ec214d850165f62e5bbaf63a0c8f
Maria Luiza <110498636+luizasal123@users.noreply.github.com>
2022-08-12 20:52:40 -0300

    Add files via upload

--- 003-projeto/app.js
+++ 003-projeto/app.js
@@ -5,3 +5,7 @@ novoSalario = salario + (aumento * salario);
 novoSalarioInflacao = novoSalario - (inflacao*novoSalario);
 console.log(novoSalario);
 console.log(novoSalarioInflacao);
+var strnovoSalarioIn=`o novo salário é: \n R$\t\t${novoSalario}`;
+var strnovoSalario = `o novo salário aplicada a inflação é : \n R$ \t\t${novoSalarioInflacao}`
+console.log(strnovoSalario)
+console.log(strnovoSalarioIn)
