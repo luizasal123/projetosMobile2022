d7d7bd62592fd70e652844094ac3e1904be9e089
Maria Luiza <110498636+luizasal123@users.noreply.github.com>
2022-08-12 19:04:33 -0300

    Add files via upload

---
+++ 003-projeto/app.js
@@ -0,0 +1,7 @@
+var salario = 2000;
+var aumento = 0.15;
+var inflacao = 0.07;
+novoSalario = salario + (aumento * salario);
+novoSalarioInflacao = novoSalario - (inflacao*novoSalario);
+console.log(novoSalario);
+console.log(novoSalarioInflacao);
