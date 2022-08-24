c4e53e3288750f00211990f2fe68d3776227c534
Maria Luiza <110498636+luizasal123@users.noreply.github.com>
2022-08-23 21:16:50 -0300

    Add files via upload

---
+++ 009-projeto/App.js
@@ -0,0 +1,31 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container1}>
+      <Text>Visao Inicial</Text>
+      <View style={styles.container2}>
+   <Text>Primeiro</Text>
+   <View style={styles.container3}>
+   <Text>Segundo{'\n'}oi</Text>
+   </View>
+       </View>
+    </View>   
+  );
+}
+
+const styles = StyleSheet.create({
+  container1: {
+    backgroundColor: 'blue',
+    margin: '5',
+  },
+  container2: {
+    backgroundColor: 'red',
+    margin: '5',
+  },
+  container3: {
+    backgroundColor: 'green',
+    margin: '5',
+  },
+});
